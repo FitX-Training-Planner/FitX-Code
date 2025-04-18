648b9c33dd87ecbf5394be5f24d39d48ed99da2f
paulohernrique <pereirapaulo436@gmail.com>
2025-04-17 23:35:30 -0300

    Criação das variáveis globais de estilo (Cores + Tamanhos de fonte).

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,27 @@
 function App() {
   return (
-    <></>  
+    <>
+      <div style={{backgroundColor: "#0056e8"}}>
+        
+      </div>
+      <div style={{backgroundColor: "#2274ff"}}>
+        a
+      </div>
+      <div style={{backgroundColor: "#002462"}}>
+        a
+      </div>
+      <div style={{backgroundColor: "#f4f4f4"}}>
+        <p style={{fontSize: "var(--small-text-size"}}>
+          A vida é feita de momentos simples. Aproveite cada um deles com alegria, coragem e um coração cheio de esperança.        </p>
+        <p style={{fontSize: "var(--text-size"}}>
+          A vida é feita de momentos simples. Aproveite cada um deles com alegria, coragem e um coração cheio de esperança.        </p>
+        <p style={{fontSize: "var(--large-text-size"}}>
+          A vida é feita de momentos simples. Aproveite cada um deles com alegria, coragem e um coração cheio de esperança.        </p>
+      </div>
+      <div style={{backgroundColor: "#000000"}}>
+        a
+      </div>
+    </>  
   );
 }
 
