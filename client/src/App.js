98825ab6c7999702296f31a6e219d6bf9cffdbc2
paulohernrique <pereirapaulo436@gmail.com>
2025-04-18 00:21:20 -0300

    Adição de estilo global. + Estilização padrão do body e dos headers.

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,26 @@ function App() {
       </div>
       <div style={{backgroundColor: "#f4f4f4"}}>
         <h1>
-          papapa@gmail.com
+          Titulo
         </h1>
+        <h2>
+          Titulo
+        </h2>
+        <h3>
+          Titulo
+        </h3>
+        <h4>
+          Titulo
+        </h4>
+        <h5>
+          Titulo
+        </h5>
+        <h6>
+          Titulo
+        </h6>
+        <p>
+          Texto normal agora que eu vou praticar programação eu estou bem feliz com isso que lindo dia é hoje
+        </p>
       </div>
       <div style={{backgroundColor: "#000000"}}>
         a
