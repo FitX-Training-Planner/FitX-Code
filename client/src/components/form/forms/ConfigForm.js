5cdeb31ca059acc7c822b67944367ca538ce4b81
paulohernrique <pereirapaulo436@gmail.com>
2025-05-14 20:08:41 -0300

    Alteração na estrutura da página create-config e seu componente de formulário.

--- client/src/components/form/forms/ConfigForm.js
+++ client/src/components/form/forms/ConfigForm.js
@@ -2,9 +2,28 @@ import Stack from "../../containers/Stack";
 import Title from "../../text/Title";
 import SubmitFormButton from "../buttons/SubmitFormButton";
 import CheckBoxInput from "../fields/CheckBoxInput";
-// import styles from "./ConfigForm.module.css";
+import PhotoInput from "../fields/PhotoInput";
+import { isPhotoValid } from "../../../utils/validators/UserValidator";
+import AlertSign from "../../messages/AlertSign";
+import styles from "./ConfigForm.module.css";
 
 function ConfigForm({ config, setConfig, handleSubmit }) {
+    function handleOnChangeUserPhoto(e) {
+        const file = e.target.files[0];
+
+        if (!file) return;
+        
+        if (!isPhotoValid(file)) return;
+        
+        const newBlobUrl = URL.createObjectURL(file);
+        
+        setConfig(prevConfig => ({
+            ...prevConfig, 
+            [e.target.name]: file,
+            photoBlobUrl: newBlobUrl
+        }));
+    };
+
     function handleOnChangeConfigData(e) {
         setConfig(prevConfig => ({
             ...prevConfig, 
@@ -15,108 +34,159 @@ function ConfigForm({ config, setConfig, handleSubmit }) {
     return (
         <form
             onSubmit={handleSubmit}
+            className={styles.config_form_container}
         >
             <Stack
-                gap="5em"
+                direction="row"
+                gap="0"
+                className={styles.config_form_container}
             >
                 <Stack
                     gap="3em"
+                    className={styles.title_and_photo_container}
                 >
-                    <Title
-                        headingNumber={2}
-                        text="Segurança e Privacidade"
-                    />
-
                     <Stack
                         gap="2em"
+                        className={styles.title_container}
                     >
-                        <CheckBoxInput
-                            name="is_complainter_anonymous"
-                            labelText="Denúncia Anônima"
-                            isChecked={config.is_complainter_anonymous}
-                            handleChange={handleOnChangeConfigData}
-                            description="Habilite para que seu perfil não seja exibido nas denúncias que você fizer."
+                        <Title
+                            headingNumber={1}
+                            text="Configuração de Perfil"
                         />
 
-                        <CheckBoxInput
-                            name="is_rater_anonymous"
-                            labelText="Avaliação Anônima"
-                            isChecked={config.is_rater_anonymous}
-                            handleChange={handleOnChangeConfigData}
-                            description="Habilite para que seu perfil não seja exibido nas avaliações que você fizer."
-                        />
-
-                        <CheckBoxInput
-                            name="is_contact_visible"
-                            labelText="Contato Visível"
-                            isChecked={config.is_contact_visible}
-                            handleChange={handleOnChangeConfigData}
-                            description="Habilite para que seu número de contato seja exibido no seu perfil."
-                        />
+                        <p>
+                            Configure seu perfil para uma experiência mais personalizada!
+                        </p>
                     </Stack>
+                    
+                    <PhotoInput
+                        name="photoFile"
+                        labelText="Foto de Perfil"
+                        size="large"
+                        blobUrl={config.photoBlobUrl}
+                        handleChange={handleOnChangeUserPhoto}
+                    />
+
+                    <p
+                        className={styles.config_alert}
+                    >
+                        <Stack
+                            direction="row"
+                            gap="0.5em"
+                        >
+                            <AlertSign
+                                varColor="--light-color"
+                                varSize="--small-text-size"
+                            />
+                            
+                            Você sempre pode alterar suas configurações após criar sua conta.
+                        </Stack>
+                    </p>
                 </Stack>
 
                 <Stack
-                    gap="3em"
+                    className={styles.config_form}
+                    gap="4em"
                 >
-                    <Title
-                        headingNumber={2}
-                        text="Notificações"
-                    />
+                    <Stack
+                        gap="3em"
+                    >
+                        <Title
+                            headingNumber={2}
+                            text="Segurança e Privacidade"
+                        />
+
+                        <Stack
+                            gap="2em"
+                        >
+                            <CheckBoxInput
+                                name="is_complainter_anonymous"
+                                labelText="Denúncia Anônima"
+                                isChecked={config.is_complainter_anonymous}
+                                handleChange={handleOnChangeConfigData}
+                                description="Habilite para que seu perfil não seja exibido nas denúncias que você fizer."
+                            />
+
+                            <CheckBoxInput
+                                name="is_rater_anonymous"
+                                labelText="Avaliação Anônima"
+                                isChecked={config.is_rater_anonymous}
+                                handleChange={handleOnChangeConfigData}
+                                description="Habilite para que seu perfil não seja exibido nas avaliações que você fizer."
+                            />
+
+                            <CheckBoxInput
+                                name="is_contact_visible"
+                                labelText="Contato Visível"
+                                isChecked={config.is_contact_visible}
+                                handleChange={handleOnChangeConfigData}
+                                description="Habilite para que seu número de contato seja exibido no seu perfil."
+                            />
+                        </Stack>
+                    </Stack>
 
                     <Stack
-                        gap="2em"
+                        gap="3em"
                     >
-                        <CheckBoxInput
-                            name="email_notification_permission"
-                            labelText="Notificações pelo E-mail"
-                            isChecked={config.email_notification_permission}
-                            handleChange={handleOnChangeConfigData}
-                            description="Habilite para que você receba nossas notificações por e-mail."
+                        <Title
+                            headingNumber={2}
+                            text="Notificações"
                         />
 
-                        <CheckBoxInput
-                            name="device_notification_permission"
-                            labelText="Notificações pelo Dispositivo"
-                            isChecked={config.device_notification_permission}
-                            handleChange={handleOnChangeConfigData}
-                            description="Habilite para que você receba nossas notificações no seu dispositivo."
-                        />
+                        <Stack
+                            gap="2em"
+                        >
+                            <CheckBoxInput
+                                name="email_notification_permission"
+                                labelText="Notificações pelo E-mail"
+                                isChecked={config.email_notification_permission}
+                                handleChange={handleOnChangeConfigData}
+                                description="Habilite para que você receba nossas notificações por e-mail."
+                            />
+
+                            <CheckBoxInput
+                                name="device_notification_permission"
+                                labelText="Notificações pelo Dispositivo"
+                                isChecked={config.device_notification_permission}
+                                handleChange={handleOnChangeConfigData}
+                                description="Habilite para que você receba nossas notificações no seu dispositivo."
+                            />
+                        </Stack>
                     </Stack>
-                </Stack>
-
-                <Stack
-                    gap="3em"
-                >
-                    <Title
-                        headingNumber={2}
-                        text="Acessibilidade"
-                    />
 
                     <Stack
-                        gap="2em"
+                        gap="3em"
                     >
-                        <CheckBoxInput
-                            name="is_dark_theme"
-                            labelText="Tema Escuro"
-                            isChecked={config.is_dark_theme}
-                            handleChange={handleOnChangeConfigData}
-                            description="Habilite para ter uma interface com cores mais escuras."
+                        <Title
+                            headingNumber={2}
+                            text="Acessibilidade"
                         />
 
-                        <CheckBoxInput
-                            name="is_english"
-                            labelText="Inglês"
-                            isChecked={config.is_english}
-                            handleChange={handleOnChangeConfigData}
-                            description="Habilite para que todo o texto do aplicativo esteja em inglês."
-                        />
+                        <Stack
+                            gap="2em"
+                        >
+                            <CheckBoxInput
+                                name="is_dark_theme"
+                                labelText="Tema Escuro"
+                                isChecked={config.is_dark_theme}
+                                handleChange={handleOnChangeConfigData}
+                                description="Habilite para ter uma interface com cores mais escuras."
+                            />
+
+                            <CheckBoxInput
+                                name="is_english"
+                                labelText="Inglês"
+                                isChecked={config.is_english}
+                                handleChange={handleOnChangeConfigData}
+                                description="Habilite para que todo o texto do aplicativo esteja em inglês."
+                            />
+                        </Stack>
                     </Stack>
-                </Stack>
 
-                <SubmitFormButton
-                    text="Confirmar"
-                />
+                    <SubmitFormButton
+                        text="Confirmar"
+                    />
+                </Stack>
             </Stack>
         </form>
     );
--- client/src/components/pages/CreateConfig.js
+++ client/src/components/pages/CreateConfig.js
@@ -1,9 +1,5 @@
 import { useState } from "react";
 import ConfigForm from "../form/forms/ConfigForm";
-import Title from "../text/Title";
-import styles from "./CreateConfig.module.css";
-import Stack from "../containers/Stack";
-import AlertSign from "../messages/AlertSign";
 
 function CreateConfig() {
     const [config, setConfig] = useState({
@@ -25,52 +21,12 @@ function CreateConfig() {
 
     return (
         <main>
-            <Stack
-                direction="row"
-                gap="0"
-                className={styles.config_page_container}
-            >
-                <Stack
-                    gap="4em"
-                    className={styles.config_form_container}
-                >
-                    <Stack
-                        gap="2em"
-                        className={styles.title_container}
-                    >
-                        <Title
-                            headingNumber={1}
-                            text="Configuração de Perfil"
-                        />
-
-                        <p>
-                            Configure seu perfil para uma experiência mais personalizada
-                        </p>
-
-                        <p
-                            className={styles.config_alert}
-                        >
-                            <Stack
-                                direction="row"
-                                gap="0.5em"
-                            >
-                                <AlertSign
-                                    varColor="--light-color"
-                                    varSize="--small-text-size"
-                                />
-                                
-                                Você sempre pode alterar suas configurações após criar sua conta 
-                            </Stack>
-                        </p>
-                    </Stack>
-
-                    <ConfigForm
-                        config={config}
-                        setConfig={setConfig}
-                        handleSubmit={handleOnSubmit}
-                    />
-                </Stack>
-
+            <ConfigForm
+                config={config}
+                setConfig={setConfig}
+                handleSubmit={handleOnSubmit}
+            />
+{/* 
                 <div
                     className={styles.config_bg}
                 >
@@ -78,8 +34,7 @@ function CreateConfig() {
                         src="images/backgrounds/create_config_bg.png"
                         alt="Background"
                     />
-                </div>
-            </Stack>
+                </div> */}
         </main>
     );
 }
