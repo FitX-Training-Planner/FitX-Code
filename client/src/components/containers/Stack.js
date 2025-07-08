4e13a42a4a562b7d3057e8abbbfd35c0c9e511ba
2007paulohenrique <pereirapaulo436@gmail.com>
2025-07-08 14:49:09 -0300

    Alteração do componente de container Stack para incluir justifyContent como propriedade.

--- client/src/components/containers/Stack.js
+++ client/src/components/containers/Stack.js
@@ -1,10 +1,10 @@
 import styles from "./Stack.module.css";
 
-function Stack({ children, gap = "1em", direction = "column", alignItems = "center", className, extraStyles = {} }) {
+function Stack({ children, gap = "1em", direction = "column", alignItems = "center", justifyContent = "space-between", className, extraStyles = {} }) {
     return (
         <div
             className={`${styles.stack} ${className || undefined}`}
-            style={{ ...extraStyles, gap, alignItems, flexDirection: direction }}
+            style={{ ...extraStyles, gap, alignItems, justifyContent, flexDirection: direction }}
         >
             {children}
         </div>
