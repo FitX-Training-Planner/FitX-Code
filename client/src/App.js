414dfa29590f220387c5453e26ee75afad708da9
2007paulohenrique <pereirapaulo436@gmail.com>
2025-07-08 16:00:15 -0300

    Instalação da biblioteca @dnd-kit/modifiers. + Criação das rotas para a página do chatbot, planos de treinos e plano de treino. + Configuração do tema do aplicativo no momento de obtenção do usuário ao abrir o app. + Estilização global do hr, mudança da cor da variável --gray-color, alteração dos tamanhos dos headers e estilização para tema escuro.

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,10 @@ import CodeConfirmation from "./components/pages/CodeConfirmation";
 import CreateConfig from "./components/pages/CreateConfig";
 import CreateTrainer from "./components/pages/CreateTrainer";
 import Login from "./components/pages/Login";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import api from "./api/axios";
 import useRequest from "./hooks/useRequest";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "./slices/user/userSlice";
 import Home from "./components/pages/Home";
 import RecoverPassword from "./components/pages/RecoverPassword";
@@ -16,32 +16,58 @@ import ModifyCardioSession from "./components/pages/ModifyCardioSession";
 import ModifyExerciseSet from "./components/pages/ModifyTrainingSet";
 import ModifyTrainingStep from "./components/pages/ModifyTrainingStep";
 import ModifyExercise from "./components/pages/ModifyTrainingExercise";
+import TrainingPlans from "./components/pages/TrainingPlans";
+import ChatBot from "./components/pages/ChatBot";
+import TrainingPlan from "./components/pages/TrainingPlan";
 
 function App() {
   const dispatch = useDispatch();
+
+  const user = useSelector(state => state.user);
+
+  const navigate = useNavigate();
 
   const location = useLocation();
   
   const { request: getUserRequest } = useRequest();
 
   const hasRun = useRef(false);
-
+  
   useEffect(() => {
     const excludedPaths = ["/login", "/create-config", "/code-confirmation", "/create-trainer", "/recover-password"];
 
     if (excludedPaths.includes(location.pathname)) return;
 
     if (hasRun.current) return;
+
     hasRun.current = true;
 
-    const postTrainer = () => api.get("/users/me");
+    const getUser = () => {
+      return api.get("/users/me");
+    };
 
-    const handleOnPostTrainerSuccess = (data) => {
+    const handleOnGetUserSuccess = (data) => {
       dispatch(setUser(data));
     };
 
-    getUserRequest(postTrainer, handleOnPostTrainerSuccess, () => undefined, "Recuperando usuário", "Usuário recuperado!", "Falha ao recuperar usuário!");
-  }, [dispatch, getUserRequest, location.pathname]);
+    const handleOnGetUserError = (err) => {
+      if (err?.response?.status === 404) {
+        navigate("/login");
+      }
+    }
+
+    getUserRequest(getUser, handleOnGetUserSuccess, handleOnGetUserError, undefined, undefined, "Falha ao recuperar usuário!");
+  }, [dispatch, getUserRequest, location.pathname, navigate]);
+
+  useEffect(() => {
+    const theme = user.config.isDarkTheme ? "dark" : "light";
+
+    document.documentElement.setAttribute("data-theme", theme);
+
+    const language = user.config.isEnglish ? "en" : "pt-BR";
+
+    document.documentElement.setAttribute("lang", language);
+  }, [user.config.isDarkTheme, user.config.isEnglish]);
 
   return (
     <Routes>
@@ -77,6 +103,16 @@ function App() {
       />
 
       <Route
+        path="/trainers/me/training-plans"
+        element={<TrainingPlans />}
+      />
+
+      <Route
+        path="/trainers/me/training-plans/:id"
+        element={<TrainingPlan />}
+      />
+
+      <Route
         path="/trainers/me/create-training-plan"
         element={<CreateTrainingPlan />}
       />
@@ -104,6 +140,11 @@ function App() {
       <Route
         path="/trainers/me/create-training-plan/modify-training-day/modify-training-step/modify-exercise/modify-set"
         element={<ModifyExerciseSet />}
+      />
+
+      <Route 
+        path="/questions-chatbot"
+        element={<ChatBot />}
       />
     </Routes>
   );
