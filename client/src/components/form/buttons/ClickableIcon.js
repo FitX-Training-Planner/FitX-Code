b15885e7dbd2a9c06667d3273c5c3db62cb5d3e8
2007paulohenrique <pereirapaulo436@gmail.com>
2025-07-08 15:00:19 -0300

    Alteração do componente ClickableIcon para comportar a mudança de tema e incluir as propriedades: size, hasTheme e isSubmit.

--- client/src/components/form/buttons/ClickableIcon.js
+++ client/src/components/form/buttons/ClickableIcon.js
@@ -1,12 +1,19 @@
 import styles from "./ClickableIcon.module.css";
+import { useEffect, useState } from "react";
 
-function ClickableIcon({ iconSrc, name, handleClick }) {
+function ClickableIcon({ iconSrc, name, handleClick, size = "medium", hasTheme = true, isSubmit = false }) {
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+    useEffect(() => {
+        setIsDarkTheme(document.documentElement.getAttribute("data-theme") === "dark");
+    }, []);
+
     return (
         <button
-            type="button"
-            className={styles.clickable_icon}
+            type={isSubmit ? "submit" : "button"}
+            className={`${styles.clickable_icon} ${styles[size]} ${isDarkTheme && hasTheme ? styles.dark_theme : undefined}`}
             title={name}
-            onClick={handleClick}
+            onClick={handleClick || undefined}
         >
             <img
                 src={iconSrc}
