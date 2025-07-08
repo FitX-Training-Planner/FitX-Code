f7a3c6739668c3bc97ac14b950eb70de08e6e6c9
2007paulohenrique <pereirapaulo436@gmail.com>
2025-07-08 15:40:12 -0300

    Criação do componente de container para a utilização do contexto do DND para arrastar itens. + Alteração do componente Sortable para que o arraste funcione somente em uma parte específica do item.

--- client/src/components/sortable/SortableItem.js
+++ client/src/components/sortable/SortableItem.js
@@ -1,25 +1,43 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import styles from "./SortableItem.module.css";
+import useWindowSize from "../../hooks/useWindowSize";
+import ClickableIcon from "../form/buttons/ClickableIcon";
 
-function SortableItem({ id, className, children }) {
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+function SortableItem({ id, className, children, extraStyles }) {
+    const { attributes, listeners, setNodeRef, setActivatorNodeRef, transform, transition } = useSortable({ id });
+
+    const { width } = useWindowSize();
 
     const style = {
         transform: CSS.Transform.toString(transform),
-        transition,
-        cursor: "grab",
-        userSelect: "none"
+        transition
     };
 
     return (
         <div
             ref={setNodeRef}
-            style={style}
-            {...attributes}
-            {...listeners}
-            className={className}
+            style={{ 
+                ...style, 
+                ...extraStyles,
+                width: (width <= 440 ? "100%" : "max-content"), 
+                maxWidth: (width <= 440 ? "unset" : "50%")
+            }}
+            className={`${styles.sortable_item} ${className}`}
         >
             {children}
+
+            <span
+                ref={setActivatorNodeRef}
+                {...attributes}
+                {...listeners}
+                title="Arrastar"
+            >
+                <ClickableIcon
+                    iconSrc="/images/icons/drag.png"
+                    name="Arrastar"
+                />
+            </span>
         </div>
     );
 }
