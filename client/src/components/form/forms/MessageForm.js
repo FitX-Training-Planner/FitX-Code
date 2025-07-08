1fd790b3126c5339b139a360287f30417c17b366
2007paulohenrique <pereirapaulo436@gmail.com>
2025-07-08 15:15:22 -0300

    Criação dos componentes de formulário para envio de mensagens no chat e envio de documentos do cliente para o treinador.

---
+++ client/src/components/form/forms/MessageForm.js
@@ -0,0 +1,39 @@
+import { formattNameAndNote } from "../../../utils/formatters/training/formatOnChange";
+import { handleOnChangeTextField } from "../../../utils/handlers/changeHandlers";
+import ClickableIcon from "../buttons/ClickableIcon";
+import MessageInput from "../fields/MessageInput";
+import Stack from "../../containers/Stack";
+import styles from "./MessageForm.module.css";
+
+function MessageForm({ chatFormContext, setChatFormContext, isChatBot, setMessageError, handleSubmit }) {
+    return (
+        <form
+            onSubmit={handleSubmit}
+        >
+            <Stack
+                direction="row"
+                className={styles.message_form}
+            >
+                <MessageInput
+                    name="message"
+                    value={chatFormContext.message}
+                    handleChange={(e) => handleOnChangeTextField(e, formattNameAndNote, undefined, chatFormContext, setChatFormContext, setMessageError)}
+                    maxLength={isChatBot ? 100 : 1000}
+                />
+
+                <Stack
+                    className={styles.send_message}
+                >
+                    <ClickableIcon
+                        iconSrc="/images/icons/send.png"
+                        name="Enviar mensagem"
+                        isSubmit
+                        hasTheme={false}
+                    />
+                </Stack>
+            </Stack>
+        </form>
+    );
+}
+
+export default MessageForm;
