88d36230a433364904af735021a227d4bdf6d12e
2007paulohenrique <pereirapaulo436@gmail.com>
2025-07-08 15:46:40 -0300

    Criação do componente de layout utilizado nas páginas que possuem um chat.

---
+++ client/src/components/layout/ChatLayout.js
@@ -0,0 +1,144 @@
+import Stack from "../containers/Stack";
+import styles from "./ChatLayout.module.css";
+import PhotoInput from "../form/fields/PhotoInput";
+import { formatDateTime } from "../../utils/formatters/text/formatDate";
+import MessageForm from "../form/forms/MessageForm";
+import SendDocumentForm from "../form/forms/SendDocumentForm";
+import React, { useEffect, useRef } from "react";
+import useWindowSize from "../../hooks/useWindowSize";
+
+function ChatLayout({ isChatBot = true, chat, chatFormContext, setChatFormContext, setMessageError, setDocumentError, handleSendDocument, handleSendMessage, chatbotLoading }) {
+    const { width } = useWindowSize();
+
+    const messagesEndRef = useRef(null);
+
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [chat.messages]);
+    
+    return (
+        <Stack 
+            className={styles.chat_container}
+        >
+            <Stack
+                direction="row"
+                className={styles.chat_info}
+            >
+                <Stack
+                    className={styles.contact_info}
+                    direction="row"
+                    gap="0.2em"
+                    justifyContent="start"
+                >
+                    <PhotoInput
+                        blobUrl={isChatBot ? "/images/icons/chatbot2.png" : chat.contact?.photoUrl}
+                        size="tiny"
+                    />
+
+                    <Stack
+                        className={styles.update_info}
+                        gap="0.2em"
+                        alignItems="start"
+                    >
+                        <span>
+                            {isChatBot ? "Coachy" : chat.contact?.name}
+                        </span>
+
+                        <span>
+                            {
+                                isChatBot 
+                                ? (chatbotLoading ? "Pensando..." : "Online") 
+                                : `Última modificação: ${formatDateTime(chat.updateDate)}`
+                            }
+                        </span>
+                    </Stack>
+                </Stack>
+
+                <Stack
+                    className={styles.chat_actions}
+                >
+                    {!isChatBot && (
+                        <Stack>
+                            <SendDocumentForm
+                                chatFormContext={chatFormContext}
+                                setChatFormContext={setChatFormContext}
+                                setDocumentError={setDocumentError}
+                                handleSubmit={handleSendDocument}
+                            />
+                        </Stack>
+                    )}
+                </Stack>
+            </Stack>
+
+            <Stack
+                className={styles.messages}
+                gap="2em"
+            >
+                {chat.messages
+                    .sort((a, b) => new Date(a.createDate) - new Date(b.createDate))
+                    .map((message, index) => (
+                        <React.Fragment
+                            key={index}
+                        >
+                            <Stack
+                                className={`${styles.message} ${message.isFromTrainer ? styles.from_trainer : undefined}`}
+                                extraStyles={{ maxWidth: width <= 440 ? "90%" : "60%" }}
+                                direction="row"
+                                alignItems="start"
+                            >
+                                <Stack
+                                    alignItems="start"
+                                    className={styles.message_content}
+                                >
+                                    <p>
+                                        {message.content}
+                                    </p>
+
+                                    <Stack
+                                        className={styles.message_info}
+                                        direction="row"
+                                    >
+                                        <span>
+                                            {formatDateTime(message.createDate)}
+                                        </span>
+
+                                        <span>
+                                            {message.isFromTrainer ? (isChatBot ? "Coachy" : chat.contact?.name) : "Você"}
+                                        </span>
+                                    </Stack>
+                                </Stack>
+
+                                {!isChatBot && (
+                                    <span
+                                        className={`${styles.view_indicator} ${message.isViewed ? styles.viewed : undefined}`}
+                                        title={message.isViewed ? "Vista" : "Não vista"}
+                                    ></span>
+                                )}
+                            </Stack>
+                        </React.Fragment>
+                    ))
+                }
+
+                <div 
+                    ref={messagesEndRef} 
+                />
+            </Stack>
+
+            <Stack
+                className={styles.message_form}
+            >
+                <MessageForm
+                    chatFormContext={chatFormContext}
+                    setChatFormContext={setChatFormContext}
+                    isChatBot={isChatBot}
+                    setMessageError={setMessageError}
+                    handleSubmit={handleSendMessage}
+                />
+            </Stack>
+        </Stack>
+    );
+}
+
+export default ChatLayout;
