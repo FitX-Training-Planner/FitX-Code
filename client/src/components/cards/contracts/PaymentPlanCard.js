522342fb7b7d85a5e91a10c7d6281f63a1594bd2
2007paulohenrique <pereirapaulo436@gmail.com>
2025-07-15 15:06:52 -0300

    Criação do cartão do plano de pagamento.

---
+++ client/src/components/cards/contracts/PaymentPlanCard.js
@@ -0,0 +1,160 @@
+import React from "react";
+import { formatPriceToBR } from "../../../utils/formatters/payments/formatOnChange";
+import convertDays from "../../../utils/formatters/text/convertDays";
+import Stack from "../../containers/Stack";
+import ClickableIcon from "../../form/buttons/ClickableIcon";
+import styles from "./PaymentPlanCard.module.css";
+import useWindowSize from "../../../hooks/useWindowSize";
+
+function PaymentPlanCard({
+    name,
+    fullPrice,
+    durationDays,
+    description,
+    benefits,
+    handleModifyPaymentPlan,
+    handleRemovePaymentPlan
+}) {
+    const { width } = useWindowSize();
+    
+    return (
+        <Stack
+            className={styles.payment_plan_card}
+        >
+            {handleModifyPaymentPlan && handleRemovePaymentPlan && (
+                <Stack
+                    direction="row"
+                    gap="0.5em"
+                >
+                    <ClickableIcon
+                        iconSrc="/images/icons/edit.png"
+                        name="Editar"
+                        handleClick={handleModifyPaymentPlan}
+                        size="small"
+                    />
+
+                    <ClickableIcon
+                        iconSrc="/images/icons/remove.png"
+                        name="Remover"
+                        handleClick={handleRemovePaymentPlan}
+                        size="small"
+                    />
+                </Stack>
+            )}
+
+            <Stack
+                gap="0.2em"
+                className={styles.payment_value}
+                extraStyles={{ fontSize: width <= 440 ? "var(--small-title-size)" : "var(--large-title-size)" }}
+            >
+                <span>
+                    {formatPriceToBR(fullPrice || "0,00")}
+                </span>
+            </Stack>
+
+            <Stack
+                gap="2em"
+            >
+                <Stack
+                    className={styles.plan_title}
+                    direction="row"
+                    justifyContent="center"
+                >
+                    {width > 440 && (
+                        <hr/>
+                    )}
+                    
+                    <span>
+                        {name}
+                    </span>
+                    
+                    {width > 440 && (
+                        <hr/>
+                    )}
+                </Stack>
+
+                <Stack
+                    gap="2em"
+                >
+                    <Stack
+                        gap="0.2em"
+                        className={styles.descriptioned_item}
+                    >
+                        <span>
+                            Duração dos Serviços:
+                        </span>
+
+                        <Stack
+                            gap="0.2em"
+                            className={styles.plan_duration}
+                        >
+                            <span>
+                                {durationDays || 0} dia{durationDays === 1 ? "" : "s"}
+                            </span>
+
+                            {durationDays > 6 && (
+                                <>
+                                    ou 
+
+                                    <span>
+                                        {convertDays(durationDays)}
+                                    </span>
+                                </>
+                            )}
+                        </Stack>
+                    </Stack>
+
+                    {benefits.length !== 0 && (
+                        <Stack
+                            className={styles.plan_benefits}
+                            alignItems="start"
+                            extraStyles={{ padding: width <= 440 ? "0" : "0 1em" }}
+                        >
+                            <span>
+                                Benefícios:
+                            </span>
+
+                            <Stack>
+                                {benefits.map((benefit, index) => (
+                                    <React.Fragment
+                                        key={index}
+                                    >
+                                        <Stack
+                                            direction="row"
+                                            alignItems="start"
+                                            justifyContent="start"
+                                        >
+                                            - 
+
+                                            <p>
+                                                {benefit.description}
+                                            </p>
+                                        </Stack>
+                                    </React.Fragment>
+                                ))}
+                            </Stack>
+                        </Stack>
+                    )}
+
+                    {description && (
+                        <Stack
+                            className={styles.payment_plan_note}
+                            alignItems="start"
+                            extraStyles={{ padding: width <= 440 ? "0" : "0 1em" }}
+                        >
+                            <span>
+                                Descrição:
+                            </span>
+
+                            <p>
+                                {description}
+                            </p>
+                        </Stack>
+                    )}
+                </Stack>
+            </Stack>
+        </Stack>
+    );
+}
+
+export default PaymentPlanCard;
