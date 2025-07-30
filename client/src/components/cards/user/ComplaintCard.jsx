7874fc13a8cd1ced992692daa59eccada34ecc51
2007paulohenrique <pereirapaulo436@gmail.com>
2025-07-29 23:00:02 -0300

    Criação dos componentes dos cards de denúncia e avaliação.

---
+++ client/src/components/cards/user/ComplaintCard.jsx
@@ -0,0 +1,111 @@
+import { useTranslation } from "react-i18next";
+import PhotoInput from "../../form/fields/PhotoInput";
+import { formatDateToExtend } from "../../../utils/formatters/text/formatDate";
+import Stack from "../../containers/Stack";
+import ClickableIcon from "../../form/buttons/ClickableIcon";
+import { formatNumberShort } from "../../../utils/formatters/text/formatNumber";
+import { useSelector } from "react-redux";
+import styles from "./TrainerPageCards.module.css";
+
+function ComplaintCard({
+    reason,
+    createDate,
+    likesNumber,
+    complainterID,
+    complainterName,
+    complainterPhotoUrl,
+    handleLike,
+    hasLiked,
+    handleRemoveComplaint
+}) {
+    const { t } = useTranslation();
+
+    const user = useSelector(state => state.user);
+    
+    return (
+        <Stack
+            className={styles.complaint_card}
+        >
+            <Stack
+                direction="row"
+                className={styles.complainter_container}
+            >
+                <Stack
+                    direction="row"
+                    justifyContent="start"
+                    gap="0.5em"
+                    className={styles.complainter}
+                >
+                    {complainterName ? (
+                        <>
+                            <PhotoInput
+                                blobUrl={complainterPhotoUrl}
+                                size="small"
+                                disabled
+                            />
+
+                            <span>
+                                {complainterName}
+                            </span>
+                        </>
+                    ) : (
+                        <p>
+                            {t("anonymousComplaint")}
+                        </p>
+                    )}
+                </Stack>
+
+                <span>
+                    {formatDateToExtend(createDate)}
+                </span>
+            </Stack>
+
+            <Stack>
+                <Stack
+                    alignItems="start"
+                >
+                    <p>
+                        {reason}
+                    </p>
+                </Stack>
+
+                <Stack
+                    direction="row"
+                    gap="2em"
+                >
+                    <Stack
+                        direction="row"
+                        justifyContent="start"
+                    >
+                        <ClickableIcon
+                            iconSrc={`/images/icons/${hasLiked ? "liked" : "like"}.png`}
+                            size="small"
+                            name={t("like")}
+                            hasTheme={false}
+                            handleClick={handleLike}
+                        />
+
+                        <span>
+                            {formatNumberShort(likesNumber)}
+                        </span>
+                    </Stack>
+
+                    {String(complainterID) === String(user.ID) && (
+                        <Stack
+                            alignItems="end"
+                        >
+                            <ClickableIcon
+                                iconSrc="/images/icons/remove.png"
+                                name={t("remove")}
+                                handleClick={handleRemoveComplaint}
+                                size="small"
+                            />
+                        </Stack>
+                    )}
+                </Stack>
+            </Stack>
+        </Stack>
+    );
+}
+
+export default ComplaintCard;
---
+++ client/src/components/cards/user/RatingCard.jsx
@@ -0,0 +1,131 @@
+import { useTranslation } from "react-i18next";
+import PhotoInput from "../../form/fields/PhotoInput";
+import { formatDateToExtend } from "../../../utils/formatters/text/formatDate";
+import Stack from "../../containers/Stack";
+import ClickableIcon from "../../form/buttons/ClickableIcon";
+import React from "react";
+import { formatNumberShort } from "../../../utils/formatters/text/formatNumber";
+import { useSelector } from "react-redux";
+import styles from "./TrainerPageCards.module.css";
+
+function RatingCard({
+    rating,
+    comment,
+    createDate,
+    likesNumber,
+    raterID,
+    raterName,
+    raterPhotoUrl,
+    handleLike,
+    hasLiked,
+    handleRemoveRating
+}) {
+    const { t } = useTranslation();
+
+    const user = useSelector(state => state.user);
+    
+    return (
+        <Stack
+            classname={styles.rating_card}
+        >
+            <Stack
+                direction="row"
+                className={styles.rater_container}
+            >
+                <Stack
+                    direction="row"
+                    justifyContent="start"
+                    gap="0.5em"
+                    className={styles.rater}
+                >
+                    {raterName ? (
+                        <>
+                            <PhotoInput
+                                blobUrl={raterPhotoUrl}
+                                size="tiny"
+                                disabled
+                            />
+
+                            <span>
+                                {raterName}
+                            </span>
+                        </>     
+                    ) : (
+                        <p>
+                            {t("anonymousRating")}
+                        </p>
+                    )}
+                </Stack>
+
+                <span>
+                    {formatDateToExtend(createDate)}
+                </span>
+            </Stack>
+
+            <Stack>
+                <Stack
+                    alignItems="start"
+                >
+                    <Stack
+                        direction="row"
+                        gap="0.5em"
+                        justifyContent="start"
+                    >
+                        {Array.from({ length: Number(rating) }, (_, index) => (
+                            <React.Fragment
+                                key={index}
+                            >
+                                <ClickableIcon
+                                    iconSrc="/images/icons/rated.png"
+                                    hasTheme={false}
+                                    size="small"
+                                />
+                            </React.Fragment>
+                        ))}
+                    </Stack>
+
+                    <p>
+                        {comment}
+                    </p>
+                </Stack>
+
+                <Stack
+                    direction="row"
+                    gap="2em"
+                >
+                    <Stack
+                        direction="row"
+                        justifyContent="start"
+                    >
+                        <ClickableIcon
+                            iconSrc={`/images/icons/${hasLiked ? "liked" : "like"}.png`}
+                            size="small"
+                            hasTheme={false}
+                            name={t("like")}
+                            handleClick={handleLike}
+                        />
+
+                        <span>
+                            {formatNumberShort(likesNumber)}
+                        </span>
+                    </Stack>
+
+                    {String(raterID) === String(user.ID) && (
+                        <Stack
+                            alignItems="end"
+                        >
+                            <ClickableIcon
+                                iconSrc="/images/icons/remove.png"
+                                name={t("remove")}
+                                handleClick={handleRemoveRating}
+                                size="small"
+                            />
+                        </Stack>
+                    )}
+                </Stack>
+            </Stack>
+        </Stack>
+    );
+}
+
+export default RatingCard;
