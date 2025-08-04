d23ec1bdacf91bf55787fff28a06f9060c7f31da
2007paulohenrique <pereirapaulo436@gmail.com>
2025-08-03 22:12:50 -0300

    Criação das rotas e consultas para recuperação de e-mail, desativação, exclusão e modificação de perfil, e modificação da foto de perfil.

--- server/app/routes/user.py
+++ server/app/routes/user.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify, request
-from ..services.user import get_user_by_id, insert_user, insert_photo
+from flask import Blueprint, jsonify, request, make_response
+from ..services.user import get_user_by_id, insert_user, insert_photo, modify_user_data, get_user_email_by_id, delete_user_account, toggle_activate_profile, modify_user_photo
+from ..utils.security import unset_jwt_cookies
 from ..database.context_manager import get_db
 from ..exceptions.api_error import ApiError
 from flask_jwt_extended import get_jwt_identity, get_jwt
@@ -79,3 +80,176 @@ def post_user():
             print(f"{error_message}: {e}")
 
             return jsonify({"message": MessageCodes.ERROR_SERVER}), 500
+
+@user_bp.route("/me/email", methods=["GET"])
+@jwt_required()
+def get_user_email():
+    error_message = "Erro na rota de recuperação de e-mail do usuário"
+
+    with get_db() as db:
+        try:
+            identity = get_jwt_identity()
+
+            jwt_data = get_jwt()
+
+            is_client = jwt_data.get("isClient")
+
+            email = get_user_email_by_id(db, identity, is_client)
+
+            return jsonify({"email": email}), 200
+
+        except ApiError as e:
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": str(e)}), e.status_code
+
+        except Exception as e:    
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": MessageCodes.ERROR_SERVER}), 500
+    
+@user_bp.route("/me/deactivate-profile", methods=["PUT"])
+@jwt_required()
+def deactivate_profile():
+    error_message = "Erro na rota de desativação de perfil do usuário"
+
+    with get_db() as db:
+        try:
+            identity = get_jwt_identity()
+
+            jwt_data = get_jwt()
+
+            is_client = jwt_data.get("isClient")
+
+            toggle_activate_profile(db, identity, is_client, False)
+
+            response = make_response("", 204)
+
+            return unset_jwt_cookies(response)
+
+        except ApiError as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": str(e)}), e.status_code
+
+        except Exception as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": MessageCodes.ERROR_SERVER}), 500
+
+@user_bp.route("/me", methods=["DELETE"])
+@jwt_required()
+def delete_account():
+    error_message = "Erro na rota de exclusão de conta do usuário"
+
+    with get_db() as db:
+        try:
+            identity = get_jwt_identity()
+
+            jwt_data = get_jwt()
+
+            is_client = jwt_data.get("isClient")
+
+            delete_user_account(db, identity, is_client)
+
+            response = make_response("", 204)
+
+            return unset_jwt_cookies(response)
+
+        except ApiError as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": str(e)}), e.status_code
+
+        except Exception as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": MessageCodes.ERROR_SERVER}), 500
+        
+@user_bp.route("/me", methods=["PUT"])
+@jwt_required()
+def modify_user():
+    error_message = "Erro na rota de modificação do usuário"
+
+    with get_db() as db:
+        try:
+            data = request.form
+
+            identity = get_jwt_identity()
+
+            jwt_data = get_jwt()
+
+            is_client = jwt_data.get("isClient")
+
+            modified_data = modify_user_data(
+                db, 
+                identity,
+                is_client,
+                data.get("name"),
+                data.get("email"),
+                data.get("isDarkTheme") == "true" if data.get("isDarkTheme") is not None else None,
+                data.get("isComplainterAnonymous") == "true" if data.get("isComplainterAnonymous") is not None else None,
+                data.get("isRaterAnonymous") == "true" if data.get("isRaterAnonymous") is not None else None,
+                data.get("emailNotificationPermission") == "true" if data.get("emailNotificationPermission") is not None else None,
+                data.get("isEnglish") == "true" if data.get("isEnglish") is not None else None
+            )
+
+            return jsonify(modified_data), 200
+
+        except ApiError as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": str(e)}), e.status_code
+
+        except Exception as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": MessageCodes.ERROR_SERVER}), 500
+
+@user_bp.route("/me/photo", methods=["PUT"])
+@jwt_required()
+def modify_photo():
+    error_message = "Erro na rota de modificação de foto do usuário"
+
+    with get_db() as db:
+        try:
+            data = request.form
+
+            photo_file = request.files.get("photoFile")
+            
+            identity = get_jwt_identity()
+
+            jwt_data = get_jwt()
+
+            is_client = jwt_data.get("isClient")
+
+            photo_url = modify_user_photo(db, identity, is_client, photo_file)
+
+            return jsonify({"photoUrl": photo_url}), 200
+
+        except ApiError as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": str(e)}), e.status_code
+
+        except Exception as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": MessageCodes.ERROR_SERVER}), 500
+
--- server/app/services/user.py
+++ server/app/services/user.py
@@ -1,5 +1,5 @@
 from ..database.models import Users, Media, Trainer
-from ..utils.user import is_email_used, hash_email, encrypt_email, hash_password
+from ..utils.user import is_email_used, hash_email, encrypt_email, hash_password, decrypt_email
 from ..utils.cloudinary import upload_file
 from sqlalchemy.orm import joinedload
 from ..exceptions.api_error import ApiError
@@ -36,10 +36,99 @@ def get_user_by_id(db, user_id, is_client):
 
         raise Exception(f"Erro ao recuperar o usuário: {e}")
 
+def delete_user_account(db, user_id, is_client):
+    try:
+        if not is_client:
+            trainer = db.query(Trainer).filter(Trainer.ID == user_id).first()
+
+            if not trainer:
+                raise ApiError(MessageCodes.TRAINER_NOT_FOUND, 404)
+            
+            user_id = trainer.fk_user_ID
+
+        user = db.query(Users).filter(Users.ID == user_id).first()
+
+        if not user:
+            raise ApiError(MessageCodes.USER_NOT_FOUND, 404)
+        
+        db.delete(user)
+
+        db.commit()
+
+        return True
+    
+    except ApiError as e:
+        print(f"Erro ao excluir conta do usuário: {e}")
+
+        raise
+    
+    except Exception as e:
+        print(f"Erro ao excluir conta do usuário: {e}")
+
+        raise Exception(f"Erro ao excluir a conta do usuário: {e}")
+
+def toggle_activate_profile(db, user_id, is_client, is_active):
+    try:
+        if not is_client:
+            trainer = db.query(Trainer).filter(Trainer.ID == user_id).first()
+
+            if not trainer:
+                raise ApiError(MessageCodes.TRAINER_NOT_FOUND, 404)
+            
+            user_id = trainer.fk_user_ID
+
+        user = db.query(Users).filter(Users.ID == user_id).first()
+
+        if not user:
+            raise ApiError(MessageCodes.USER_NOT_FOUND, 404)
+        
+        user.is_active = is_active
+
+        db.commit()
+
+        return True if user.is_active else False
+    
+    except ApiError as e:
+        print(f"Erro ao alterar atributo de perfil ativo do usuário: {e}")
+
+        raise
+    
+    except Exception as e:
+        print(f"Erro ao alterar atributo de perfil ativo do usuário: {e}")
+
+        raise Exception(f"Erro ao alterar o atributo de perfil ativo do usuário: {e}")
+    
+def get_user_email_by_id(db, user_id, is_client):
+    try:
+        if not is_client:
+            trainer = db.query(Trainer).filter(Trainer.ID == user_id).first()
+
+            if not trainer:
+                raise ApiError(MessageCodes.TRAINER_NOT_FOUND, 404)
+            
+            user_id = trainer.fk_user_ID
+
+        user = db.query(Users).filter(Users.ID == user_id).first()
+
+        if not user:
+            raise ApiError(MessageCodes.USER_NOT_FOUND, 404)
+
+        return decrypt_email(user.email_encrypted)
+    
+    except ApiError as e:
+        print(f"Erro ao recuperar e-mail do usuário pelo id: {e}")
+
+        raise
+    
+    except Exception as e:
+        print(f"Erro ao recuperar e-mail do usuário pelo id: {e}")
+
+        raise Exception(f"Erro ao recuperar o e-mail do usuário pelo id: {e}")
+
 def get_user_id_by_email(db, email):
     email_hash = hash_email(email)
 
-    user = db.query(Users).options(joinedload(Users.trainer)).filter(Users.email_hash == email_hash).first()
+    user = db.query(Users).filter(Users.email_hash == email_hash).first()
 
     if user is None:
         raise ApiError(MessageCodes.USER_EMAIL_NOT_EXISTS, 404)
@@ -102,6 +191,95 @@ def insert_user(
 
         raise Exception(f"Erro ao criar o usuário: {e}")
 
+def modify_user_data(
+    db,
+    user_id,
+    is_client,
+    name = None,
+    email = None,
+    isDarkTheme = None,
+    isComplainterAnonymous = None,
+    isRaterAnonymous = None,
+    emailNotificationPermission = None,
+    isEnglish = None
+):
+    try:
+        if not is_client:
+            trainer = db.query(Trainer).filter(Trainer.ID == user_id).first()
+
+            if not trainer:
+                raise ApiError(MessageCodes.TRAINER_NOT_FOUND, 404)
+            
+            user_id = trainer.fk_user_ID
+
+        user = db.query(Users).filter(Users.ID == user_id).first()
+        
+        if user is None:
+            raise ApiError(MessageCodes.USER_NOT_FOUND, 404)
+
+        updated_fields = {
+            "user": {
+                "config": {}
+            }
+        }
+
+        if name is not None:
+            user.name = name
+
+            updated_fields["user"]["name"] = user.name
+
+        if email is not None:
+            if is_email_used(db, email):
+                raise ApiError(MessageCodes.ERROR_EMAIL_USED, 409)
+
+            email_hash = hash_email(email)
+
+            email_encrypted = encrypt_email(email)
+
+            user.email_encrypted = email_encrypted
+            user.email_hash = email_hash
+            
+            updated_fields["email"] = email
+
+        if isDarkTheme is not None:
+            user.is_dark_theme = isDarkTheme
+
+            updated_fields["user"]["config"]["isDarkTheme"] = user.is_dark_theme
+        
+        if isComplainterAnonymous is not None:
+            user.is_complainter_anonymous = isComplainterAnonymous
+
+            updated_fields["user"]["config"]["isComplainterAnonymous"] = user.is_complainter_anonymous
+
+        if isRaterAnonymous is not None:
+            user.is_rater_anonymous = isRaterAnonymous
+
+            updated_fields["user"]["config"]["isRaterAnonymous"] = user.is_rater_anonymous
+
+        if emailNotificationPermission is not None:
+            user.email_notification_permission = emailNotificationPermission
+
+            updated_fields["user"]["config"]["emailNotificationPermission"] = user.email_notification_permission
+
+        if isEnglish is not None:
+            user.is_english = isEnglish
+
+            updated_fields["user"]["config"]["isEnglish"] = user.is_english
+
+        db.commit()
+
+        return updated_fields
+
+    except ApiError as e:
+        print(f"Erro ao modificar usuário: {e}")
+
+        raise
+
+    except Exception as e:
+        print(f"Erro ao modificar usuário: {e}")
+
+        raise Exception(f"Erro ao modificar o usuário: {e}")
+
 def insert_photo(db, photo_file):
     try:
         url = upload_file(photo_file)
@@ -119,14 +297,55 @@ def insert_photo(db, photo_file):
         return new_media.ID
 
     except ApiError as e:
-        print(f"Erro ao salvar foto: {e}")
+        print(f"Erro ao salvar foto de perfil do usuário: {e}")
 
         raise
 
     except Exception as e:
-        print(f"Erro ao salvar foto: {e}")
+        print(f"Erro ao salvar foto de perfil do usuário: {e}")
 
-        raise Exception(f"Erro ao salvar a foto: {e}")
+        raise Exception(f"Erro ao salvar a foto de perfil do usuário: {e}")
+
+def modify_user_photo(db, user_id, is_client, photo_file):
+    try:
+        if not is_client:
+            trainer = db.query(Trainer).filter(Trainer.ID == user_id).first()
+
+            if not trainer:
+                raise ApiError(MessageCodes.TRAINER_NOT_FOUND, 404)
+            
+            user_id = trainer.fk_user_ID
+
+        user = db.query(Users).filter(Users.ID == user_id).first()
+        
+        if user is None:
+            raise ApiError(MessageCodes.USER_NOT_FOUND, 404)
+
+        url = upload_file(photo_file)
+
+        new_media = Media(
+            url=url
+        )
+
+        db.add(new_media)
+        
+        db.flush()
+
+        user.fk_media_ID = new_media.ID
+        
+        db.commit()
+
+        return url
+
+    except ApiError as e:
+        print(f"Erro ao modificar foto de perfil do usuário: {e}")
+
+        raise
+
+    except Exception as e:
+        print(f"Erro ao modificar foto de perfil do usuário: {e}")
+
+        raise Exception(f"Erro ao modificar a foto de perfil do usuário: {e}")
     
 def modify_user_password(db, user_id, new_password): 
     try:
