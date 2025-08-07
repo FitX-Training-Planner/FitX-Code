2940af192babaeb4fc4f012e60beb4fc1d216336
2007paulohenrique <pereirapaulo436@gmail.com>
2025-08-07 15:37:27 -0300

    Criação da rota do webhook do Mercado Pago.

--- server/app/routes/security.py
+++ server/app/routes/security.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request, render_template, make_response, redirect
 from ..database.context_manager import get_db
+from ..database.models import PaymentTransaction, PlanContract, ContractStatus
 from ..exceptions.api_error import ApiError
 from ..utils.security import check_login, generate_code, verify_code, set_jwt_cookies, set_jwt_access_cookies, unset_jwt_cookies
 from ..utils.user import is_email_used, send_email
@@ -10,9 +11,12 @@ from ..utils.client_decorator import only_client
 from ..utils.message_codes import MessageCodes
 from ..config import MercadopagoConfig
 from ..services.trainer import insert_mercadopago_trainer_info
+from ..services.client import get_valid_mp_token
 import requests
 import os
-from datetime import datetime, timedelta, timezone
+from sqlalchemy.orm import joinedload, subqueryload
+from datetime import datetime, timedelta, timezone, date
+import mercadopago
 
 security_bp = Blueprint("security", __name__)
 
@@ -189,21 +193,20 @@ def refresh_token():
 def logout():
     error_message = "Erro na rota de logout"
 
-    with get_db() as db:
-        try:
-            response = make_response("", 204)
+    try:
+        response = make_response("", 204)
 
-            return unset_jwt_cookies(response)
+        return unset_jwt_cookies(response)
 
-        except ApiError as e:
-            print(f"{error_message}: {e}")
+    except ApiError as e:
+        print(f"{error_message}: {e}")
 
-            return jsonify({"message": str(e)}), e.status_code
+        return jsonify({"message": str(e)}), e.status_code
 
-        except Exception as e:
-            print(f"{error_message}: {e}")
+    except Exception as e:
+        print(f"{error_message}: {e}")
 
-            return jsonify({"message": MessageCodes.ERROR_SERVER}), 500
+        return jsonify({"message": MessageCodes.ERROR_SERVER}), 500
 
 @security_bp.route("/mercadopago/connect/<int:trainer_id>", methods=["GET"])
 @jwt_required()
@@ -269,6 +272,97 @@ def oauth_callback():
             frontend_redirect_url = f"{frontend_url}/me"
 
             return redirect(frontend_redirect_url)
+        
+        except ApiError as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": str(e)}), e.status_code
+
+        except Exception as e:
+            db.rollback()
+
+            print(f"{error_message}: {e}")
+
+            return jsonify({"message": MessageCodes.ERROR_SERVER}), 500
+        
+@security_bp.route("/mercadopago/webhook", methods=["POST"])
+def mercadopago_webhook():
+    error_message = "Erro na rota de recebimento de notificação do mercado pago"
+
+    with get_db() as db:
+        try:
+            args = request.args
+
+            payment_id = args.get("id") or args.get("data.id")
+
+            if not payment_id:
+                raise ApiError(MessageCodes.MP_PAYMENT_ID_NOT_FOUND, 400)
+
+            fallback_sdk = mercadopago.SDK(os.getenv("MP_CLIENT_ACCESS_TOKEN"))
+            fallback_result = fallback_sdk.payment().get(payment_id)
+
+            if fallback_result["status"] != 200:
+                raise ApiError(MessageCodes.MP_PAYMENT_NOT_FOUND, 500)
+
+            payment_info = fallback_result["response"]
+
+            preference_id = (
+                payment_info.get("order", {}).get("preference_id") or
+                payment_info.get("preference_id")
+            )
+
+            if not preference_id:
+                raise ApiError(MessageCodes.MP_PREFERENCE_ID_NOT_FOUND, 400)
+            
+            transaction = (
+                db.query(PaymentTransaction)
+                .options(joinedload(PaymentTransaction.payment_plan))
+                .filter(PaymentTransaction.mp_preference_id == preference_id)
+                .first()
+            )
+
+            if not transaction:
+                raise ApiError(MessageCodes.TRANSACTION_NOT_FOUND, 404)
+
+            sdk = mercadopago.SDK(get_valid_mp_token(db, transaction.fk_trainer_ID))
+            
+            result = sdk.payment().get(payment_id)
+
+            if result["status"] != 200:
+                raise ApiError(MessageCodes.MP_PAYMENT_NOT_FOUND, 500)
+
+            payment_info = result["response"]
+            status = payment_info["status"]
+            mp_transaction_id = str(payment_info["id"])
+            receipt_url = payment_info.get("receipt_url")
+
+            if status == "approved":
+                transaction.is_finished = True
+                transaction.mp_transaction_id = mp_transaction_id
+                transaction.receipt_url = receipt_url
+                transaction.create_date = datetime.now()
+
+                new_contract = PlanContract(
+                    start_date = date.today(),
+                    end_date = date.today() + timedelta(days=transaction.payment_plan.duration_days),
+                    fk_user_ID = transaction.fk_user_ID,
+                    fk_trainer_ID = transaction.fk_trainer_ID,
+                    fk_payment_plan_ID = transaction.fk_payment_plan_ID,
+                    fk_payment_transaction_ID = transaction.ID,
+                    fk_contract_status_ID = (
+                        db.query(ContractStatus)
+                        .filter(ContractStatus.name == "Ativo")
+                        .scalar()
+                    )
+                )
+
+                db.add(new_contract)
+
+                db.commit()                
+
+            return "", 201
         
         except ApiError as e:
             db.rollback()
