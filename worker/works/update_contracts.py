460f4b763e5c57d4d972e4a83b2f03c61387a879
2007paulohenrique <pereirapaulo436@gmail.com>
2025-08-08 00:37:46 -0300

    Criação do serviço agendado para atualizar contratos vencidos.

---
+++ worker/works/update_contracts.py
@@ -0,0 +1,41 @@
+from datetime import date
+from ..database.models import ContractStatus, PlanContract
+from ..database.context_manager import get_db
+from sqlalchemy.orm import joinedload
+
+def run():
+    try:
+        with get_db() as db:
+            active_status = (
+                db.query(ContractStatus)
+                .filter(ContractStatus.name == "Ativo")
+                .first()
+            )
+            expired_Status = (
+                db.query(ContractStatus)
+                .filter(ContractStatus.name == "Vencido")
+                .first()
+            )
+
+            contracts = (
+                db.query(PlanContract)
+                .join(ContractStatus)
+                .options(
+                    joinedload(PlanContract.user)
+                )
+                .filter(
+                    PlanContract.end_date < date.today(),
+                    PlanContract.fk_contract_status_ID == active_status.ID
+                )
+                .all()
+            )       
+           
+            for contract in contracts:
+                contract.fk_contract_status_ID = expired_Status.ID
+
+                contract.user.fk_training_plan_ID = None
+
+            db.commit()
+
+    except Exception as e:
+        print(f"Erro ao atualizar contratos vencidos: {e}")
