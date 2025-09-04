0b1aac84a5f2b64c0e3e688cefb592b566c79cdd
2007paulohenrique <pereirapaulo436@gmail.com>
2025-09-04 19:44:54 -0300

    Tratamento de erro na função de criação de preferência de pagamento do Mercado Pago.

--- server/app/utils/mercadopago.py
+++ server/app/utils/mercadopago.py
@@ -7,55 +7,61 @@ from ..config import MercadopagoConfig
 brazil_tz = ZoneInfo("America/Sao_Paulo")
 
 def create_payment_preference(mp_user_id, item_id, title, description, price, app_fee, payer_email, payer_first_name, transaction_id, expiration_seconds):
-    sdk = mercadopago.SDK(MercadopagoConfig.MP_CLIENT_ACCESS_TOKEN)
+    try:
+        sdk = mercadopago.SDK(MercadopagoConfig.MP_CLIENT_ACCESS_TOKEN)
 
-    frontend_base_url = os.getenv("FRONT_END_URL")
-    api_url = os.getenv("API_URL")
+        frontend_base_url = os.getenv("FRONT_END_URL")
+        api_url = os.getenv("API_URL")
 
-    start_time = datetime.now(brazil_tz)
-    end_time = start_time + timedelta(seconds=expiration_seconds)
+        start_time = datetime.now(brazil_tz)
+        end_time = start_time + timedelta(seconds=expiration_seconds)
 
-    preference_data = {
-        "items": [
-            {
-                "id": item_id,
-                "title": title,
-                "description": description,
-                "quantity": 1,
-                "currency_id": "BRL",
-                "unit_price": float(price + app_fee)
-            }
-        ],
-        "payer": {
-            "first_name": payer_first_name,
-            "email": payer_email
-        },
-        "back_urls": {
-            "success": f"{frontend_base_url}/payment/success",
-            "failure": f"{frontend_base_url}/payment/failure",
-            "pending": f"{frontend_base_url}/payment/pending",
-        },
-        "auto_return": "all",
-        "external_reference": str(transaction_id),
-        "binary_mode": True,
-        "purpose": "wallet_purchase",
-        "application_fee": float(app_fee),
-        "collector_id": mp_user_id, 
-        "payment_methods": {
-            "excluded_payment_types": [
-                { "id": "ticket" },
-                { "id": "atm" } 
-            ]
-        },
-        "notification_url": f"{api_url}/mercadopago/webhook",
-        "expires": True,
-        "expiration_date_from": start_time.isoformat(),
-        "expiration_date_to": end_time.isoformat(),
-    }
+        preference_data = {
+            "items": [
+                {
+                    "id": item_id,
+                    "title": title,
+                    "description": description,
+                    "quantity": 1,
+                    "currency_id": "BRL",
+                    "unit_price": float(price + app_fee)
+                }
+            ],
+            "payer": {
+                "first_name": payer_first_name,
+                "email": payer_email
+            },
+            "back_urls": {
+                "success": f"{frontend_base_url}/payment/success",
+                "failure": f"{frontend_base_url}/payment/failure",
+                "pending": f"{frontend_base_url}/payment/pending",
+            },
+            "auto_return": "all",
+            "external_reference": str(transaction_id),
+            "binary_mode": True,
+            "purpose": "wallet_purchase",
+            "application_fee": float(app_fee),
+            "collector_id": mp_user_id, 
+            "payment_methods": {
+                "excluded_payment_types": [
+                    { "id": "ticket" },
+                    { "id": "atm" } 
+                ]
+            },
+            "notification_url": f"{api_url}/mercadopago/webhook",
+            "expires": True,
+            "expiration_date_from": start_time.isoformat(),
+            "expiration_date_to": end_time.isoformat(),
+        }
 
-    preference_response = sdk.preference().create(preference_data)
+        preference_response = sdk.preference().create(preference_data)
 
-    return preference_response["response"]
+        return preference_response["response"]
+
+    except Exception as e:
+        print(f"Erro na função de criação da preferência de pagamento do Mercado Pago: {e}")
+
+        raise 
 
 def calculate_app_fee_in_payment_plan(full_price):
     if full_price <= 300:
